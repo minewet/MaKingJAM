798f3f35f66c06b4738f2fa23f566772df86079b
SEON UI LEE <sunnyinsummer@ewhain.net>
2021-12-27 16:19:05 +0900

    option, continue & quit button

---
+++ Game/Assets/Scripts/GameManage/GameManagement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagement : MonoBehaviour
+{
+    public GameObject Setting;
+    public GameObject quitButton;
+
+    public void OnclickButton()
+    {
+        if(PlayerPrefs.GetFloat("oncePlayed") > 0.0f)
+        {
+            PlayerPrefs.SetFloat("OncePlayed", 0.0f);
+        }
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setting.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (Setting.activeSelf)
+            {
+                Setting.SetActive(false);
+            }
+            else
+            {
+                Setting.SetActive(true);
+            }
+
+        }
+    }
+}
