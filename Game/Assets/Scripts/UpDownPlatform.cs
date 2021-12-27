9928f128adc0996265825dc4cf25e91755328d49
Juyie <ghkdwndl1004@ewhain.net>
2021-12-27 14:23:31 +0900

    플랫폼 상하좌우 이동 스크립트 추가

---
+++ Game/Assets/Scripts/UpDownPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform up;
+
+    [SerializeField]
+    private Transform down;
+
+    [SerializeField]
+    private float speed;
+
+    private float direction = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < down.position.y)
+            direction *= -1;
+        else if (gameObject.transform.position.y > up.position.y)
+            direction *= -1;
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + Time.deltaTime * speed * direction, gameObject.transform.position.z);
+    }
+}
