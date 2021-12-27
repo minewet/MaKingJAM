a12945ad7c2d0e58af0b210fe1b840d576aa8f42
SEON UI LEE <sunnyinsummer@ewhain.net>
2021-12-28 08:53:43 +0900

    스테이지 관리 변수 management에 넣음

--- Game/Assets/Scripts/GameManage/Management.cs
+++ Game/Assets/Scripts/GameManage/Management.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Management
 {
+    // 엔딩 분기점
     public static int A = 0;
     public static int B = 0;
     public static int C = 0;
@@ -11,4 +12,9 @@ public class Management
     public static int E = 0;
     public static int F = 0;
 
+    // 스테이지
+    public static int staff = 0; // 사람으로 스테이지 끝날때마다 +1
+    public static int cat = 0; // 고양이로 스테이지 끝날때마다 +1
+    // staff = 3, stage1로 돌아가서 고양이로 플레이
+    // cat = 3, ending
 }
