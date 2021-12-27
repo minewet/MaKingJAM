48a22d005c82d2ad8b0cf17ab492ef9eb5874f07
seungmi <smlee0102@gmail.com>
2021-12-28 03:10:40 +0900

    Chap3 맵구성

---
+++ Game/Assets/Scripts/PlatformFalling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFalling : MonoBehaviour
+{
+
+    public float fall = 0.3f, destroy = 2f;
+    Rigidbody2D pf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pf = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+	{
+        Invoke("FallPlatform", fall);
+    }
+
+    void FallPlatform()
+	{
+        pf.isKinematic = false;
+    }
+}
