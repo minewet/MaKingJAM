1b2744b42ac4de823fc6670d3bbcc16393b6ac11
Juyie <ghkdwndl1004@ewhain.net>
2021-12-28 11:32:37 +0900

    텍스트 상호작용 사운드 적용

---
+++ Game/Assets/Scripts/Chapter1/ChooseBGM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseBGM : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip staff;
+
+    [SerializeField]
+    private AudioClip cat;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if(Management.staff == 0)
+        {
+            audioSource.clip = staff;
+        }
+        else if(Management.staff == 3)
+        {
+            audioSource.clip = cat;
+        }
+
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
